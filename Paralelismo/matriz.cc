e1013c2a565c9b0738cccaa8bbbeff85a17d1b75
Luis1155 <luis1155alberto@gmail.com>
2018-09-26 12:38:58 -0500

    Matriz-Dijkstra

--- Paralelismo/matriz.cc
+++ Paralelismo/matriz.cc
@@ -14,8 +14,8 @@ using namespace std;
     Para compilar: g++ -std=c++11 -o matriz matriz.cc
     Para ejecutar: ./matriz
 */
-// #define g INT_MAX / 3
-#define g 100
+#define g INT_MAX
+// #define g 100
 int minimo(int a, int b)
 {
     if (a < b)
@@ -24,7 +24,7 @@ int minimo(int a, int b)
         return b;
 }
 
-void printMatrix(vector<vector<int>> A)
+void printMatrix(vector<vector<int>>& A)
 {
     int rows_A = A.size();
     int cols_A = A[0].size();
@@ -33,13 +33,16 @@ void printMatrix(vector<vector<int>> A)
     {
         for (int j = 0; j < cols_A; j++)
         {
-            cout << A[i][j] << " ";
+            if(A[i][j] == g)
+                cout<<"g ";
+            else
+                cout << A[i][j] << " ";
         }
         cout << "\n";
     }
 }
 
-vector<vector<int>> matrixmult(vector<vector<int>> A, vector<vector<int>> B)
+vector<vector<int>> matrixmult(vector<vector<int>>& A, vector<vector<int>>& B)
 {
     int rows_A = A.size();
     int cols_A = A[0].size();
@@ -60,12 +63,17 @@ vector<vector<int>> matrixmult(vector<vector<int>> A, vector<vector<int>> B)
     for (int i = 0; i < rows_A; i++)
         for (int j = 0; j < cols_B; j++)
             for (int k = 0; k < cols_A; k++)
-                C[i][j] = minimo(C[i][j], A[i][k] + B[k][j]);
+            {   
+                if(A[i][k] == g || B[k][j] == g)
+                    C[i][j] = minimo(C[i][j], g);    
+                else
+                    C[i][j] = minimo(C[i][j], A[i][k] + B[k][j]);
+            }
 
     return C;
 }
 
-vector<vector<int>> strange(vector<vector<int>> G)
+vector<vector<int>> strange(vector<vector<int>>& G)
 {
     int nodes = G.size();
     int cont = 0;
@@ -81,34 +89,35 @@ vector<vector<int>> strange(vector<vector<int>> G)
     return A;
 }
 
-vector<vector<int>> strange2(vector<vector<int>> G)
+vector<vector<int>> strange2(vector<vector<int>>& G)
 {
     bool flag = false;
     int nodes = G.size();
     int cont = 0;
-    vector<vector<int>> A;
-    vector<vector<int>> B;
-
+    vector<vector<int>> A = G;
+    vector<vector<int>> path;
+    
+    cout<<"condicional"<<endl;
+    
     if (nodes % 2 != 0)
     {
         flag = true;
         nodes = nodes - 1;
-        B = G;
     }
 
     while (nodes != 1)
     {
-        A = matrixmult(G, G);
-        G = A;
+        A = matrixmult(A, A);
         nodes = nodes / 2;
         cont++;
     }
 
     if (flag == true)
     {
-        A = matrixmult(B, G);
+        A = matrixmult(G, A);
         cont++;
     }
+
     cout<<"Number of multiplications (Strange2): "<<cont<<endl;
 
     return A;
@@ -119,30 +128,38 @@ int main()
     vector<vector<int>> A;
     vector<vector<int>> X;
 
-    // A = {{0, 1, 3, g, g, g, g, g},
-    //      {5, 0, 1, 8, g, g, g, g},
-    //      {g, 9, 0, g, 8, g, g, g},
-    //      {g, g, g, 0, g, g, g, g},
-    //      {g, g, 7, g, 0, g, 2, 7},
-    //      {g, 1, g, 4, g, 0, 7, g},
-    //      {g, g, 7, g, g, g, 0, g},
-    //      {g, g, g, g, g, 1, g, 0}};
-    A = {{0, 1, 3, g, g, g, g, g, 3, 6, g},
-         {5, 0, 1, 8, g, g, g, g, 1, 8, g},
-         {g, 9, 0, g, 8, g, g, g, g, 7, g},
-         {g, g, g, 0, g, g, g, g, g, g, 7},
-         {g, g, 7, g, 0, g, 2, 7, 5, g, 2},
-         {g, 1, g, 4, g, 0, 7, g, g, 2, 7},
-         {4, g, 7, g, g, g, 0, g, 4, 8, g},
-         {g, g, g, g, g, 1, g, 0, g, 2, 7},
-         {g, g, 7, g, 5, g, 2, 7, 0, g, 2},
-         {g, g, 7, g, 3, g, 5, 7, g, 0, 3},
-         {3, 1, 3, g, g, g, g, g, 3, 6, 0}};
+    // A = {{0, g, 3, g},
+    //      {g, 0, g, g},
+    //      {g, g, 0, 1},
+    //      {3, 1, g, 0}};
+         
+    A = {{0, 6, g, g, 6, g, g, g},
+         {4, 0, g, g, g, g, g, g},
+         {g, g, 0, g, 2, 9, 7, g},
+         {g, g, g, 0, g, g, g, g},
+         {g, g, 4, g, 0, g, g, g},
+         {g, g, g, g, g, 0, g, 9},
+         {g, g, g, g, 6, 2, 0, g},
+         {g, g, g, g, g, g, g, 0}};
 
+    // A = {{0, 1, 3, g, g, g, g, g, 3, 6, g},
+        //  {5, 0, 1, 8, g, g, g, g, 1, 8, g},
+        //  {g, 9, 0, g, 8, g, g, g, g, 7, g},
+        //  {g, g, g, 0, g, g, g, g, g, g, 7},
+        //  {g, g, 7, g, 0, g, 2, 7, 5, g, 2},
+        //  {g, 1, g, 4, g, 0, 7, g, g, 2, 7},
+        //  {4, g, 7, g, g, g, 0, g, 4, 8, g},
+        //  {g, g, g, g, g, 1, g, 0, g, 2, 7},
+        //  {g, g, 7, g, 5, g, 2, 7, 0, g, 2},
+        //  {g, g, 7, g, 3, g, 5, 7, g, 0, 3},
+        //  {3, 1, 3, g, g, g, g, g, 3, 6, 0}};
+ 
     X = strange(A);
     printMatrix(X);
     cout<<"\n";
     X = strange2(A);
     printMatrix(X);
+    cout<<"\n!!!!!!!!!!!!Matriz original!!!!!!!!!!!!!!!!!"<<endl;
+    printMatrix(A);
     return 0;
 }
